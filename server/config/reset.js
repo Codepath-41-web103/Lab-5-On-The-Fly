5c0b768b81a5f56d1d7da94ff8b70903129025b6
Wisesofthemall <foxxydieujuste@gmail.com>
2023-10-16 16:46:36 -0400

    task: Complete Connection to Database

--- server/config/reset.js
+++ server/config/reset.js
@@ -1,55 +1,57 @@
-import { pool } from "../config/database.js";
-import "../config/dotenv.js";
-import giftData from "../data/gifts.js";
+import { pool } from "./database.js";
+import "./dotenv.js";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+import fs from "fs";
+const currentPath = fileURLToPath(import.meta.url);
+const tripsFile = fs.readFileSync(
+  path.join(dirname(currentPath), "../config/data.json"),
+);
+const tripsData = JSON.parse(tripsFile);
 
-const createGiftsTable = async () => {
-  const createTableQuery = `
-    DROP TABLE IF EXISTS events;
-
-    CREATE TABLE IF NOT EXISTS events (
-      id SERIAL PRIMARY KEY,
-      name VARCHAR(255) NOT NULL,
-      location VARCHAR(255) NOT NULL,
-      groupname VARCHAR(255) NOT NULL,
-      imagelink VARCHAR(255) NOT NULL,
-      date VARCHAR(255) NOT NULL,
-      arenaimage VARCHAR(255) NOT NULL
-    );
+const createTripsTable = async () => {
+  const createTripsTableQuery = `
+      CREATE TABLE IF NOT EXISTS trips (
+          id serial PRIMARY KEY,
+          title varchar(100) NOT NULL,
+          description varchar(500) NOT NULL,
+          img_url text NOT NULL,
+          num_days integer NOT NULL,
+          start_date date NOT NULL,
+          end_date date NOT NULL,
+          total_cost money NOT NULL
+      );
   `;
-
   try {
-    await pool.query(createTableQuery);
-    console.log("🎉 events table created successfully");
+    const res = await pool.query(createTripsTableQuery);
+    console.log("🎉 trips table created successfully");
   } catch (err) {
-    console.error("⚠️ error creating events table", err);
+    console.error("⚠️ error creating trips table", err);
   }
 };
-
-const seedGiftsTable = async () => {
-  await createGiftsTable();
-
-  giftData.forEach((gift) => {
+const seedTripsTable = async () => {
+  await createTripsTable();
+  tripsData.forEach((trip) => {
     const insertQuery = {
-      text: "INSERT INTO events (name, location, groupname, imagelink, date, arenaimage) VALUES ($1, $2, $3, $4, $5, $6)",
+      text: "INSERT INTO trips (title, description, img_url, num_days, start_date, end_date, total_cost) VALUES ($1, $2, $3, $4, $5, $6, $7)",
     };
-
     const values = [
-      gift.name,
-      gift.location,
-      gift.groupName,
-      gift.imageLink,
-      gift.date,
-      gift.arenaImage,
+      trip.title,
+      trip.description,
+      trip.img_url,
+      trip.num_days,
+      trip.start_date,
+      trip.end_date,
+      trip.total_cost,
     ];
-
     pool.query(insertQuery, values, (err, res) => {
       if (err) {
-        console.error("⚠️ error inserting events", err);
+        console.error("⚠️ error inserting trip", err);
         return;
       }
-      console.log(`✅ ${gift.name} added successfully`);
+
+      console.log(`✅ ${trip.title} added successfully`);
     });
   });
 };
-
-seedGiftsTable();
+seedTripsTable();
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,23 +1,25 @@
-import { pool } from '../config/database.js'
+import { pool } from "../config/database.js";
 
 const getUsers = async (req, res) => {
-try {
-    const results = await pool.query('SELECT * FROM users ORDER BY id ASC')
-    res.status(200).json(results.rows)
+  try {
+    const results = await pool.query("SELECT * FROM users ORDER BY id ASC");
+    res.status(200).json(results.rows);
   } catch (error) {
-    res.status(400).json( { error: error.message } )  
+    res.status(400).json({ error: error.message });
   }
-}
+};
 
 const getUserById = async (req, res) => {
   try {
-    const userId = parseInt(req.params.id)
-    const results = await pool.query('SELECT * FROM users WHERE id=$1', [userId])
-    res.status(200).json(results.rows[0])
+    const userId = parseInt(req.params.id);
+    const results = await pool.query("SELECT * FROM users WHERE id=$1", [
+      userId,
+    ]);
+    res.status(200).json(results.rows[0]);
   } catch (error) {
-    res.status(400).json( { error: error.message } )
+    res.status(400).json({ error: error.message });
   }
-}
+};
 
 // inner join - retrieve all the appointments for a specific user and include
 // information about the hairstyle and hair stylist for each appointment
@@ -28,20 +30,19 @@ const getUserAppointments = async (req, res) => {
     INNER JOIN hair_stylists ON appointments.stylist_id = hair_stylists.id
     INNER JOIN hair_styles ON appointments.style_id = hair_styles.id
     WHERE appointments.user_id = $1
-  `
-  
+  `;
+
   try {
-    const userId = parseInt(req.params.id)
-    const results = await pool.query(query, [userId])
-    res.status(200).json(results.rows)
+    const userId = parseInt(req.params.id);
+    const results = await pool.query(query, [userId]);
+    res.status(200).json(results.rows);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
-  catch (error) {
-    res.status(400).json( { error: error.message } )
-  }
-}
+};
 
 export default {
   getUsers,
   getUserById,
-  getUserAppointments
-}
+  getUserAppointments,
+};
