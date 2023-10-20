2a802b6523c31b57835e0325f2df1efb62e8a445
Mike Odnis <airwolf635@gmail.com>
2023-10-19 21:46:03 -0400

    Quick commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,15 @@
-import { Router, Route } from 'react-router-dom' 
+import { Routes, Route } from 'react-router-dom' 
 import './App.scss'
 import { TripDestinations, Activities, Destinations, Trips } from './pages/index'
 function App() {
-
   return (
     <>
-      <Router>
+      <Routes>
         <Route path="/" exact element={<TripDestinations />} />
         <Route path="/" exact element={<Destinations />} />
         <Route path="/" exact element={<Activities />} />
         <Route path="/" exact element={<Trips />} />
-      </Router>
+      </Routes>
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './globals.css'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { DarkThemeToggle, Flowbite } from 'flowbite-react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
+    <Flowbite>
+      <BrowserRouter>
+        <DarkThemeToggle />
+        <App />
+      </BrowserRouter>
+    </Flowbite>
+    </React.StrictMode>,
 )
