09bfc8f3edb29e8f4fa6c0b1bc608dbb91c6b4f3
Wisesofthemall <foxxydieujuste@gmail.com>
2023-11-01 15:33:15 -0400

    chore: Reset Files to default state

--- client/src/pages/auth/Auth.tsx
+++ client/src/pages/auth/Auth.tsx
@@ -1,24 +1,17 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import ForgotPassword from "../auth/ForgotPassword/ForgotPassword";
-import Login from "./Login/Login.jsx";
-import SignUp from "./Signup/Signup.jsx";
-import { useAuth } from "../../auth/AuthState";
+import Login from "../auth/Login/Login";
+import SignUp from "../auth/Signup/Signup";
 export default function Auth() {
-  const user = useAuth();
-
-  useEffect(() => {
-    if (user) {
-      //! Navigate to the chat app
-    } else {
-      //! Navigate to the login page
-    }
-  }, []);
+  const Login = () => {
+    return <></>;
+  };
   return (
     <>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<SignUp />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<SignUp />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </>
--- client/src/pages/auth/Login/Login.jsx
+++ client/src/pages/auth/Login/Login.jsx
@@ -1,58 +1,6 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
 import React from "react";
-import { firebaseAuth } from "../../../auth/Firebase";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const handleEmailAndPassword = async (e) => {
-    try {
-      await signInWithEmailAndPassword(firebaseAuth, email, password);
-      toast.success("Signup successful");
-    } catch (error) {
-      console.log(error.message);
-      toast.error(error.message);
-    }
-  };
-  return (
-    <div className="w-full h-full grid place-items-center">
-      <div className=" grid place-items-center w-4/5 h-4/5">
-        <div className="">Login</div>
-        <Input
-          id="email"
-          label="Email"
-          value={email}
-          stateChange={setEmail}
-          required
-        />
-        <Input
-          id="password"
-          label="Password"
-          value={password}
-          stateChange={setPassword}
-          required
-        />
-        <button
-          onClick={handleEmailAndPassword}
-          className="relative
-       rounded-lg
-        hover:opacity-80
-        transition w-full bg-white border-black text-black py-1 text-sm border-[1px] font-light "
-        >
-          <div className="">Signup</div>
-        </button>
-        <div className="flex">
-          <div className="text-sm text-gray-500 mx-1">
-            {" "}
-            Already have an account
-          </div>
-          <div className="text-sm text-gray-500 mx-1 hover:underline">
-            {" "}
-            Login
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div>Login</div>;
 };
 export default Login;
